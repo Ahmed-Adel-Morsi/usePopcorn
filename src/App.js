cc5d83b590f55fa545348f938831243cf9d196ed
Ahmed Adel <ahmedadel0239@gmail.com>
2026-02-18 01:54:56 +0200

    Update API URLs to use HTTPS for improved security

--- src/App.js
+++ src/App.js
@@ -244,7 +244,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         setError("");
 
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`,
         );
 
         if (!res.ok)
