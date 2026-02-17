6cc9bf9f972de26e044a2c4eb2b5973fff5f6e3b
Ahmed Adel <ahmedadel0239@gmail.com>
2026-02-18 01:49:37 +0200

    Final Version

--- src/useMovies.js
+++ src/useMovies.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const KEY = "5836e50d";
+const KEY = process.env.REACT_APP_API_KEY;
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
